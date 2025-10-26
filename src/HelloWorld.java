705dcf427e4464f6ec71347e4ea2eb53070a1f1c
Greeshma594 <greeshmagokuldas369@gmail.com>
2025-10-26 15:47:17 +0530

    Update and rename Hello.java to HelloWorld.java

---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class HelloWorld extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h1>Hello World from MyApp v1.0.0</h1>");
+    }
+}
+
